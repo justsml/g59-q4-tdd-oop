3f04e83957b706c350a99a81ad08592eeb8b3af7
Jason Dulin <jas.dulin@gmail.com>
2017-11-14 15:03:15 -0700

    12 tests passing

--- cars/car.js
+++ cars/car.js
@@ -1,9 +1,18 @@
 module.exports = Car
 
 function Car(mpg) {
-
+  this.odometer = 0
+  this.gallons = 0
+  this.mpg = 10
+  this.trips = []
 }
 
 Car.prototype.fill = function(gallons) {
+  this.gallons += gallons
+}
 
+Car.prototype.drive = function(odometer) {
+  this.odometer += odometer
+  this.gallons -= (odometer/this.mpg)
+  this.trips.push(odometer + ' miles')
 }
--- dmv/dmv.js
+++ dmv/dmv.js
@@ -1,7 +1,22 @@
 function DMV(agents) {
+  this.agents = agents
+  this.customers = []
 };
 
-DMV.prototype.customersInLine = function () {
+DMV.prototype.customersInLine = function (customer) {
+  this.customer = customer
 };
+
+DMV.prototype.enter = function () {
+  this.customers.push(this.agents)
+}
+
+DMV.prototype.currentCustomerFor = function () {
+
+}
+
+DMV.prototype.nextCustomer = function () {
+
+}
 
 module.exports = DMV;
