5614c83ff6d3db310ecd496cb20393e1da8c577b
James Mendenhall <Nattysoccer9@yahoo.com>
2017-11-15 15:11:34 -0700

    Initial commit

--- robots/robot.js
+++ robots/robot.js
@@ -1,9 +1,29 @@
 function Robot(startCharge) {
-
+  this._position = [0,0]
+  this._battery = startCharge
+  this._history = []
 }
 
 Robot.prototype.position = function() {
+  return this._position
+}
 
+Robot.prototype.moveUp = function() {
+  this._position[1] += 1;
+  this._history.push('up')
+}
+
+Robot.prototype.moveLeft = function() {
+ this._position[0] -= 1;
+ this._history.push('left')
+}
+
+Robot.prototype.battery = function(num) {
+  return this._battery
+}
+
+Robot.prototype.history = function() {
+  return this._history
 }
 
 module.exports = Robot
