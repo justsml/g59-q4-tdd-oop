829708bf883285136f8f16b960417ca8f903e637
STEVE KORNAHRENS <steve.kornahrens@gmail.com>
2017-11-15 15:15:44 -0700

    for now

--- cars/car.js
+++ cars/car.js
@@ -5,5 +5,22 @@ function Car(mpg) {
 }
 
 Car.prototype.fill = function(gallons) {
-
+  this.gallons += gallons
 }
+
+//yesterday
+function Car({make, model, year, fuel}) {
+  this.make = make
+  this.model = model
+  this.year = year
+  this.fuel = fuel
+}
+
+Car.prototype.addFuel = function(fuel) {
+  this.fuel += fuel
+}
+
+const tacoma = new Car({make: "Toyota", model: "Tacoma", year: 2014, fuel: 100})
+
+tacoma.addFuel(10)
+console.log(tacoma.fuel)
--- dmv/dmv.js
+++ dmv/dmv.js
@@ -1,7 +1,38 @@
 function DMV(agents) {
-};
+  this.agents = agents
+  this.customers = []
+  this.inline = []
+}
 
-DMV.prototype.customersInLine = function () {
-};
+DMV.prototype.customersInLine = function(customer) {
+  return this.inline.push(customer)
+}
+
+DMV.prototype.currentCustomerFor = function(agent) {
+  assignedCustomers.forEach(customer => {
+    var assignment = this.customer
+    if(assignment.agent == agent) {
+      return assignment.customer
+    }
+  })
+
+  return null
+}
+
+DMV.prototype.nextCustomer = function() {
+  //cj way
+  let nextAvailableAgent = this.agents.shift()
+  let nextCustomer = this.customersInLine.shift()
+  this.assignedCustomers.push({
+    agent: nextAvailableAgent,
+    customer: nextCustomer
+  })
+
+  //Dan way
+  // let customers = this.customers.filter(customer => !customer.agent)
+  // let lastCust = customers[0]
+  // let agentIndex = this.customers.length % this.agents.length
+  // lastCust.agent = this.agents[agentIndex]
+}
 
 module.exports = DMV;
