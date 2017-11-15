acf15a1799adf8e9757f33f83ba1faf970641c38
Dan Levy <dan@danlevy.net>
2017-11-15 15:02:34 -0700

    WIP: dmv in progress

--- dmv/dmv.js
+++ dmv/dmv.js
@@ -1,7 +1,49 @@
 function DMV(agents) {
-};
+  this.agents = agents;
+  this.customers = []
+}
+DMV.prototype.customersInLine = function() {
+  return this.customers
+  .filter(customer => !customer.agent)
+  .map(customer => customer.customer)
+}
+DMV.prototype.enter = function(customer) {
+  this.customers.push({customer, agent: null})
+}
+DMV.prototype.currentCustomerFor = function(agent) {
+  let customersServed = this.customers
+    .filter(customer => customer.agent === agent)
 
-DMV.prototype.customersInLine = function () {
-};
+  console.log('customersServed', customersServed)
+  if (customersServed.length === 0) {
+    return null
+  }
+  return customersServed[0].customer
 
-module.exports = DMV;
+}
+/**
+ * 1. Find customers without 'agent' key
+ * 2. Pick last customer
+ * 3. Find next agent (using modulo magic)
+ * 4. Assigning agent to the customer Object
+ */
+DMV.prototype.nextCustomer = function() {
+  let customers = this.customers.filter(customer => !customer.agent)
+  let lastCust = customers[0]
+  let agentIndex = this.customers.length % this.agents.length
+  lastCust.agent = this.agents[agentIndex]
+  console.log('agent', agentIndex, this.agents[agentIndex])
+  console.log('Customer', lastCust)
+  /**
+   * lastCust should look like:
+   * {
+   *   customer: 'Drivin Dave',
+   *   agent: 'Surly'
+   * }
+   */
+}
+
+
+
+module.exports = DMV
+
