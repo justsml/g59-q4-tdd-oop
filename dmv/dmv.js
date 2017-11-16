5bc9d870f763d75d3b3c5057b73b22711cb59957
Sadathe Toure <trms75@gmail.com>
2017-11-16 07:20:30 -0700

    Dmv and robot 1st commit

--- dmv/dmv.js
+++ dmv/dmv.js
@@ -1,7 +1,45 @@
 function DMV(agents) {
+  this.agents = agents;
+  this.customers = []
 };
 
 DMV.prototype.customersInLine = function () {
+  return this.customers
+  .filter(customer => !customer.agent)
+  .map(customer => customer.customer)
 };
+
+DMV.prototype.enter =function(customer) {
+  this.customers.push({customer, agent: null})
+};
+
+DMV.prototype.currentCustomerFor =function(agent) {
+  let customersServed = this.customers
+  .filter(customer => customer.agent == agent)
+  if (customersServed.length == 0) {
+    return null
+  }
+  return customerServed[0].customer
+};
+/**
+*1. find customers without 'agent key'
+*2. pick last customer
+*3. find next agent (using modulo magic)
+*4. assigning agent to the customer object
+*/
+DMV.prototype.nextCustomer =function () {
+  let customers = this.customers.filter(customer => !customer.agent)
+  let lastCust = customers[0]
+  let agentIndex = this.customers.length % this.agents.length
+  lastCust.agent = this.agents[agentIndex]
+  console.log('agent', agentIndex, this.agents[agentIndex]);
+  console.log('customer', lastCust);
+  /**
+  * lastCust should look like:
+  {}
+  *
+  */
+};
+
 
 module.exports = DMV;
