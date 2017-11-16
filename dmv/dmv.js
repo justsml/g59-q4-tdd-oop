fc8fcf5df7419055b1669950d046393d1284e6be
Jason Dulin <jas.dulin@gmail.com>
2017-11-16 10:50:47 -0700

    Started dmv exercise

--- dmv/dmv.js
+++ dmv/dmv.js
@@ -3,20 +3,30 @@ function DMV(agents) {
   this.customers = []
 };
 
-DMV.prototype.customersInLine = function (customer) {
-  this.customer = customer
+DMV.prototype.customersInLine = function () {
+  return this.customers
+  .filter(customer => !customer.agent)
+  .map(customer => customer.customer)
 };
 
-DMV.prototype.enter = function () {
-  this.customers.push(this.agents)
+DMV.prototype.enter = function (customer) {
+  this.customers.push({customer})
 }
 
-DMV.prototype.currentCustomerFor = function () {
-
+DMV.prototype.currentCustomerFor = function (agent) {
+  let customersServed = this.customers
+  .filter(customer => customer.agent === agent)
+  if (customersServed.length === 0) {
+    return null
+  }
+  return customersServed[0].customer
 }
 
 DMV.prototype.nextCustomer = function () {
-
+  let customers = this.customersInLine()
+  let lastCust = customers[customers.length - 1]
+  let agentIndex = this.customers.length % this.agents.length
+  lastCust.agent = this.agents[agentIndex]
 }
 
 module.exports = DMV;
