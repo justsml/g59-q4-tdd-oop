2aab3ac4e246ab1c54b23f671ed798183dbc47ae
Lindsay Chapin <lnchapin@gmail.com>
2017-11-15 17:40:53 -0700

    WIP only directory left

--- dmv/dmv.js
+++ dmv/dmv.js
@@ -1,13 +1,49 @@
 function DMV(agents) {
-  this.DMV = agents
+  this.agents = agents
+  this.inLine = []
+  this.assignedCustomers = []
 };
 
-DMV.prototype.customersInLine = function (customer) {
-  console.log(customer);
+DMV.prototype.customersInLine = function () {
+  return this.inLine
 };
 
 DMV.prototype.enter = function (customer) {
-  customersInLine.push(customer)
+  this.inLine.push(customer)
+}
+
+DMV.prototype.nextCustomer = function () {
+  let nextAvailiableAgent = this.agents.shift()
+  let nextCustomer = this.inLine.shift()
+
+  this.assignedCustomers.push({
+    agent: nextAvailiableAgent,
+    customer: nextCustomer
+  })
+}
+
+DMV.prototype.currentCustomerFor = function (agent) {
+  for (var i = 0; i < this.assignedCustomers.length; i++) {
+		let assignment = this.assignedCustomers[i];
+
+		if (assignment.agent === agent) {
+			return assignment.customer;
+		}
+	}
+	return null;
+}
+
+DMV.prototype.resolve = function (customer) {
+  //pull the agent out of the object and put back into the agents
+  //delete the object that has the customer name from the array
+  for (var i = 0; i < this.assignedCustomers.length; i++) {
+    let assignment = this.assignedCustomers[i];
+
+    if (assignment.customer === customer) {
+      this.agents.push(assignment.agent)
+      this.assignedCustomers.splice(1)
+    }
+  }
 }
 
 module.exports = DMV;
