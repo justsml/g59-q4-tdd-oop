6dc3cba5ab41fa228dba66661cb770cad789f612
Christopher White <Maverick@Christophers-MacBook-Air.local>
2017-11-17 09:53:04 -0700

    car works, robot works, DMV near complete

--- cars/car.js
+++ cars/car.js
@@ -1,9 +1,18 @@
-module.exports = Car
-
 function Car(mpg) {
+  this.gallons = 0
+  this.odometer = 0
+  this.mpg = mpg
+  this.trips = []
+}
 
+Car.prototype.drive = function(miles) {
+  this.odometer = this.odometer + miles
+  this.trips.push(miles + ' miles')
+  this.gallons = this.gallons - miles / 10
 }
 
 Car.prototype.fill = function(gallons) {
-
+  this.gallons = this.gallons + gallons
 }
+
+module.exports = Car
