5bce047fe18ba83fb03450c95e0c863c5b1b9970
Jason Stroup <jasonstroup12@gmail.com>
2017-11-17 13:48:20 -0700

    initial commit

--- dmv/dmv.js
+++ dmv/dmv.js
@@ -4,4 +4,15 @@ function DMV(agents) {
 DMV.prototype.customersInLine = function () {
 };
 
+DMV.prototype.resolve = function (customer) {
+  this.agentL
+  if (this.attending[agent] == customer)
+  this.attending[agent] = null
+
+}
+
+
+
+
+
 module.exports = DMV;
