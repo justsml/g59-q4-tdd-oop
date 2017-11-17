0b0fea97f6b77609ca5c0956c528cc0ea8ea74a8
Nate <n8bdoesemailvia@gmail.com>
2017-11-17 15:11:42 -0700

    somewhat complete

--- cars/car.js
+++ cars/car.js
@@ -1,9 +1,21 @@
 module.exports = Car
 
-function Car(mpg) {
+function Car({mpg, odometer, trips, gallons}) {
 
+    // if (!(this instanceof Car)) { return new Car({mpg, odometer, trip, gallons}) }
+
+    this.mpg = mpg || 10,
+    this.odometer = odometer || 0,
+    this.trips = trips || [],
+    this.gallons = gallons || 0
 }
 
 Car.prototype.fill = function(gallons) {
+    this.gallons += gallons
+}
 
+Car.prototype.drive = function(miles) {
+    this.gallons -= (miles / this.mpg),
+    this.trips.push(miles+" miles"),
+    this.odometer += miles
 }
--- dmv/dmv.js
+++ dmv/dmv.js
@@ -1,7 +1,43 @@
 function DMV(agents) {
+  this.agents = agents
+  this.inLine = []
+  this.assignedCustomers = [];
+
+
 };
 
+
 DMV.prototype.customersInLine = function () {
+  return this.inLine
+};
+
+DMV.prototype.enter = function(customer){
+  this.inLine.push(customer)
+};
+
+DMV.prototype.currentCustomerFor = function (agent){
+  for(var i = 0; i < this.assignedCustomers.length; i++){
+  var assignment =  this.assignedCustomers[i];
+  if(assignment.agent == agent){
+    return assignment.customer;
+  }
+  }
+  return null;
+
+  // return this.assignedCustomers.filter((assignment) => {
+  //   return assignment.agent == agent;
+  // })[0].customer;
+};
+
+DMV.prototype.nextCustomer = function (){
+  var nextAvailableAgent = this.agents.shift();
+  var nextCustomer = this.inLine.shift();
+  this.assignedCustomers.push({
+    agent: nextAvailableAgent,
+    customer: nextCustomer
+  });
+
+  return null;
 };
 
 module.exports = DMV;
