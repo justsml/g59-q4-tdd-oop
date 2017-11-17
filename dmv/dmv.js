12c5ad4c44cd7544c896c98da7b452281c10e57b
Dan Levy <dan@danlevy.net>
2017-11-16 21:29:36 -0700

    dmv, nailed it!!!

--- dmv/dmv.js
+++ dmv/dmv.js
@@ -1,49 +1,38 @@
-function DMV(agents) {
-  this.agents = agents;
-  this.customers = []
-}
-DMV.prototype.customersInLine = function() {
-  return this.customers
-  .filter(customer => !customer.agent)
-  .map(customer => customer.customer)
-}
-DMV.prototype.enter = function(customer) {
-  this.customers.push({customer, agent: null})
-}
-DMV.prototype.currentCustomerFor = function(agent) {
-  let customersServed = this.customers
-    .filter(customer => customer.agent === agent)
-
-  console.log('customersServed', customersServed)
-  if (customersServed.length === 0) {
-    return null
-  }
-  return customersServed[0].customer
-
-}
-/**
- * 1. Find customers without 'agent' key
- * 2. Pick last customer
- * 3. Find next agent (using modulo magic)
- * 4. Assigning agent to the customer Object
- */
-DMV.prototype.nextCustomer = function() {
-  let customers = this.customers.filter(customer => !customer.agent)
-  let lastCust = customers[0]
-  let agentIndex = this.customers.length % this.agents.length
-  lastCust.agent = this.agents[agentIndex]
-  console.log('agent', agentIndex, this.agents[agentIndex])
-  console.log('Customer', lastCust)
-  /**
-   * lastCust should look like:
-   * {
-   *   customer: 'Drivin Dave',
-   *   agent: 'Surly'
-   * }
-   */
-}
-
-
-
 module.exports = DMV
 
+function DMV(agents) {
+  this.agentList = agents
+  this.attending = {}
+  this.customers = []
+}
+
+DMV.prototype.enter = function(customer) {
+  this.customers.push(customer)
+}
+
+DMV.prototype.customersInLine = function() {
+  return this.customers
+}
+
+DMV.prototype.currentCustomerFor = function(agent) {
+  return this.attending[agent] ? this.attending[agent] : null
+}
+
+DMV.prototype.nextCustomer = function() {
+  const currentCustomer = this.customers.shift()
+  this.agentList.find(agent => {
+    if (!this.attending[agent]) {
+      this.attending[agent] = currentCustomer
+      return true
+    }
+  })
+}
+
+DMV.prototype.resolve = function(customer) {
+  this.agentList.find(agent => {
+    if (this.attending[agent] === customer) {
+      this.attending[agent] = null
+      return true
+    }
+  })
+}
