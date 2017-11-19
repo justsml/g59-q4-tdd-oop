5eeff973616b6e5793ae1f4980e8424c1a3e1810
Chelcie Britt <chelciebritt@Chelcies-MacBook-Pro.local>
2017-11-18 18:39:04 -0700

    a few pass

--- cars/car.js
+++ cars/car.js
@@ -1,9 +1,23 @@
-module.exports = Car
+module.exports = Car;
 
-function Car(mpg) {
-
-}
+function Car(mpg) {}
 
 Car.prototype.fill = function(gallons) {
+  this.gallons += gallons;
+};
 
+function Car({ make, model, year, fuel }) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.fuel = fuel;
 }
+
+Car.prototype.addFuel = function(fuel) {
+  this.fuel += fuel;
+};
+
+const ford = new Car({ make: 'Ford', model: 'Escape', year: 2013, fuel: 100 });
+
+ford.addFuel(10);
+console.log(ford.fuel);
