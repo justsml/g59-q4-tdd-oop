e563893ab90d3e9bf9773bfc4fee955bb558f97b
Miriam Quintana <quintana.miriam.cc@gmail.com>
2017-11-22 20:48:18 -0700

    Car passing

--- cars/car.js
+++ cars/car.js
@@ -1,9 +1,69 @@
-module.exports = Car
+// car.fill()
+// car.drive()
+// CAR.GALLONS
+// CAR.ODOMETER
+// CAR.TRIPS
 
-function Car(mpg) {
 
+
+module.exports = Car;
+function Car({ mpg, gallons, odometer, trips }) {
+  if (!(this instanceof Car)) {
+    return new Car({ mpg, gallons, odometer, trips });
+  }
+  this.gallons = 0;
+  this.odometer = 0;
+  this.trips = [];
+  this.mpg = 10;
 }
 
 Car.prototype.fill = function(gallons) {
+  this.gallons += gallons;
+};
 
-}
+Car.prototype.drive = function(distance) {
+  this.gallons -= distance / this.mpg;
+  this.odometer += distance;
+  this.trips.push(distance + " miles");
+};
+
+
+// function Car(mpg) {
+//   this.fill = mpg;
+//   this.trips = [ ]
+//   this.odometer = 0
+// }
+//
+// Car.prototype.fill = function(gallons) {
+//   this._gallons += gallons
+// }
+//
+// Car.prototype.trips = function() {
+//   return this._trips
+// }
+//
+// Car.prototype.odometer = function() {
+//   this._trips.push('odometer')
+//   return this._odometer
+// }
+//
+// Car.prototype.drive = function() {
+//   this._trips.push('odometer')
+//   return this._drive
+// }
+//
+//
+// module.exports = Car
+
+
+
+// function Car(mpg) {
+//   this._fill = mpg;
+//   this._odometer = 0
+//   this._trips = []
+// }
+//
+// Car.prototype.drive = function() {
+//   this._trips.push('odometer')
+//   return this._drive
+// }
--- dmv/dmv.js
+++ dmv/dmv.js
@@ -1,7 +1,42 @@
+// DMV.CUSTOMERSINLINE()
+// DMV.CURRENTCUSTOMERFOR()
+// dmv.enter()
+// dmv.nextcustomer()
+// dmv.resolve()******************
+
 function DMV(agents) {
+  this.agents = agents;
+  this.customers = []
 };
 
 DMV.prototype.customersInLine = function () {
+  return this.customers
+  .filter(customer => !customer.agent)
+  .map(customer => customer.customer)
 };
+
+DMV.prototype.enter =function(customer) {
+  this.customers.push({customer, agent: null})
+};
+
+DMV.prototype.currentCustomerFor = function(agent) {
+  let customersServed = this.customers
+  .filter(customer => customer.agent == agent)
+  if (customersServed.length == 0) {
+    return null
+  }
+  return customerServed[0].customer
+};
+
+DMV.prototype.nextCustomer =function () {
+  let customers = this.customers.filter(customer => !customer.agent)
+  let lastCust = customers[0]
+  let agentIndex = this.customers.length % this.agents.length
+  lastCust.agent = this.agents[agentIndex]
+  console.log('agent', agentIndex, this.agents[agentIndex]);
+  console.log('customer', lastCust);
+
+};
+
 
 module.exports = DMV;
